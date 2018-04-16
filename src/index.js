4719da736816913e1323b58b3bd6f2246be62706
Robin Devouge <rnd@emakina.com>
2018-04-16 11:23:16 +0200

    Fix path to recipes.json

--- src/index.js
+++ src/index.js
@@ -5,15 +5,17 @@ import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
 function getJson(uri) {
-    fetch(uri).then((response) => { 
+    fetch(uri).then((response) => {
+      console.log(response);
+
         return response.json();
     }).then((j) => {
-        console.log(j); 
+        console.log(j);
         return j;
     });
 }
 
-const recipes = getJson('./recipes.json');
+const recipes = getJson(`${process.env.PUBLIC_URL}/recipes.json`);
 
 ReactDOM.render( < App/ > , document.getElementById('root'));
 registerServiceWorker();
